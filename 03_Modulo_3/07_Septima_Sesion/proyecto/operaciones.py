3b8fd7d459443b4492325122f907ee9d126e1874
Luisguillermomolero <luisguillermomolero@gmail.com>
2025-02-11 21:38:34 -0500

    commit

---
+++ 03_Modulo_3/07_Septima_Sesion/proyecto/operaciones.py
@@ -0,0 +1,18 @@
+# operaciones.py
+
+from productos import obtener_producto
+
+def calcular_total(lista_productos):
+    """Calcula el total de la compra."""
+    total = 0
+    for producto in lista_productos:
+        total += producto['precio']
+    return total
+
+def agregar_producto(id_producto, cantidad, lista_productos):
+    """Agrega un producto a la lista de compra."""
+    producto = obtener_producto(id_producto)
+    if producto:
+        lista_productos.append({"nombre": producto["nombre"], "precio": producto["precio"] * cantidad})
+    else:
+        raise ValueError(f"Producto con ID {id_producto} no encontrado.")
