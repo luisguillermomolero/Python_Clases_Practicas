dfa0ad81a76f8d5dd75bf25655027113e6b09a2c
Luisguillermomolero <luisguillermomolero@gmail.com>
2025-03-11 21:52:16 -0500

    commit

--- 03_Modulo_3/01_Primera_Sesion/Clase.py
+++ 03_Modulo_3/01_Primera_Sesion/Clase.py
@@ -1,141 +1,117 @@
-# App que permita dividir dos números
 """
-# Con captura de excepciones
+# Excepciones
 
-def division_cero(numero1, numero2):
+def division_segura(numero1, numero2):
     try:
-        resultado = numero1 / numero2
-        print(f"El resultado es {resultado}")
-    except ZeroDivisionError as e:
-        print("La división entre Cero no se puede lograr")
+        resultado = numero1/numero2
+        print("El resultado de la división es: ", resultado)
+    except ZeroDivisionError:
+        print("No es posible desarrollar esta división")
         return None
 
-division_cero(2,0)
+division_segura(10, 0)
 
-# Sin captura de excepciones
+# Manejo avanzado de excepciones
 
-def division_cero(numero1, numero2):
-        resultado = numero1 / numero2
-        print(f"El resultado es {resultado}")
-division_cero(2,0)
-
-# Mismo ejercicio explicando las excepciones múltiples
-
-def division_segura():
+def division_segura_dos():
     try:
-        numerador = int(input("Ingrese por favor el numerador de la división: "))
-        denominador = int(input("Ingrese por favor el denominador de la división: "))
-        resultado =  numerador / denominador
-        print(f"El resultado de la división de {numerador} entre el {denominador} es igual a: {resultado}")
+        numerador = int(input("Ingrese el numerador: "))
+        denominador = int(input("Ingrese el denominador: "))
+        resultado = numerador / denominador
+        print(f"El resultado de la división es: {resultado}")
     except (ZeroDivisionError, ValueError) as e:
-        print(f"Error {e}")
+        print(f"Error:  {e}")
 
-division_segura()
+division_segura_dos()
 
 
-# Manejo de excepciones específicas "Exception" << No es recomendable SIEMPRE ya que puede esconder errores >>
+try:
+    with open("datos.txt", "r") as archivo:
+        contenido = archivo.read()
+        numero = int(contenido.strip())
+        resultado = 100 / numero
+        print(f"El resultado es: {resultado}")
+except Exception as e:
+    print(f"Se genero un ERROR: {e}")
 
-def abrir_archivo():
+
+
+# try, except, else, finally
+
+try:
+    numero = int(input("Ingrese un número: "))
+    resultado = 10 / numero
+except ValueError as e:
+    print(f"Error: Entrada invalida. {e}")
+except ZeroDivisionError as e:
+    print(f"Error: No se puede desarrollar una división entre cero:")
+else:
+    print(f"El resultad es: {resultado}")
+finally:
+    print("Ejercicio finalizado")
+    
+
+
+def procesar_pedido(codigo_producto, cantidad):
     try:
-        with open("datos.txt", "r") as archivo:
-            contenido =  archivo.read()
-            numero = int(contenido.strip())
-            print(numero)
-    except Exception as e:
-        print(f"Se produjo un error{e}")
-
-abrir_archivo()
-
-
-# Uso del else y finally
-
-# Ejercicio de división por Cero
-
-def division_completa():
-    try:
-        numero = int(input("Ingrese un número: "))
-        resultado = 10 / numero
-    except ValueError as e:
-        print(f"Error: {e}")
-    except ZeroDivisionError as e:
-        print(f"Error: {e}")
-    else:
-        print(f"El resultado de la división es {resultado}")
-    finally:
-        print("La operación finalizo...")
-
-division_completa()
-
-# App que permite procesar pedidos
-# Validar que el código de producto zea alfanumerico
-# Validar que la canitidad zea mayor a Cero
-
-def procesar_pedido():
-    try:
-        codigo_producto = input("Ingrese el código del producto: ")
-        cantidad = int(input("Ingrese la cantidad del producto: "))
-        
-        # Validar que el código de producto zea alfanumerico
+        # Simular una validación de datos
         if not codigo_producto.isalnum():
-            raise ValueError("El código del producto debe zer alfanumerico")
-        
-        # Validar que la cantidad zea mayor a Cero
+            # raise ERROR: gestiona de forma manual y en tiempo de ejecucuión un ERROR
+            raise ValueError("El código del producto debe ser alfanúmerico")
         if cantidad <= 0:
-            raise ValueError("La cantidad del producto debe ser mayor a Cero")
+            raise ValueError("La cantidad debe ser superior a cero")
         
-        precio_unitario = 50
+        # Simular el calculo del total
+        precio_unitario = 100 # ejemplo del precio por unidad
         total = precio_unitario * cantidad
     
     except ValueError as e:
         print(f"Error al procesar el pedido: {e}")
     else:
-        print(f"Total a pagar: {total}")
+        print(f"Pedido procesado exitosamente. El total a pagar es: ${total:.2f}")
     finally:
-        print("Operación finalizada")
+        print("Operación finalizada. Registro actualizado")
 
-procesar_pedido()
+# método principal
 
+if __name__ == "__main__":
+    procesar_pedido("ABC123", 2)
+    procesar_pedido("######", 12)
+    procesar_pedido("GHJ8547", -10)
+    procesar_pedido("GHJ8547", 10.25)
+    
 """
 
-# Excepciones personalizadas << Último ejercicio >>
-
-
 class ErrorDePago(Exception):
-    """ Gestón de excepciones"""
     pass
 
-class PasarelaDePago():
-    """Simulación una estrategia tecnológica de pago"""
-    
+class PasarelaDePago:
     @staticmethod
     def procesar_pago(numero_tarjeta, monto):
-        
         if not numero_tarjeta.startswith("4"):
-            raise ErrorDePago("El número de la tarjeta no es valido")
+            raise ErrorDePago("El número de la tarjeta no es valido. Debe iniciar en 4")
         if monto <= 0:
-            raise ErrorDePago("El monto debe ser mayor a cero")
+            raise ErrorDePago("El monto de la compra debe ser superior a cero")
+        if len(numero_tarjeta) != 16:
+            raise ErrorDePago("El número de la tarjeta debe tener 16 digitos")
         
-        return f"Pago de ${monto} fue procesado con éxito"
+        return f"El pago de ${monto} fue procesado con éxito."
 
-def procesar_pago_cliente(nombre_cliente, numero_tarjeta, monto):
+def proceso_pago_cliente(nombre_cliente, numero_tarjeta, monto):
     try:
-        print(f"Iniciando el proceso de pago para {nombre_cliente}")
+        print(f"Iniciando el proceso de pago de {nombre_cliente}...")
         resultado = PasarelaDePago.procesar_pago(numero_tarjeta, monto)
     except ErrorDePago as e:
-        print(f"Error al procesar el pago. {e}")
+        print(f"Error al procesar el pago: {e}")
     except Exception as e:
-        print(f"Se produjo un error inesperado. {e}")
+        print(f"Se produjo un error inesperado: {e}")
     else:
         print(resultado)
     finally:
-        print("Registro finalizado")
+        print("El registro de la transacción finalizo.")
 
 if __name__ == "__main__":
-    #procesar_pago_cliente("Jose", "432156", 99.80)
-    #procesar_pago_cliente("Luis", "12345", 100)
-    procesar_pago_cliente("Carolina", "456789", 0)
+    #proceso_pago_cliente("Luis Molero", "4111111111111111", 456.52)
+    #proceso_pago_cliente("Andrés Serrano", "5111111111111111", 80.23)
+    proceso_pago_cliente("Alfonso Cantillo", "4123541256327458", -250)
     
-    
-        
-        
-        
